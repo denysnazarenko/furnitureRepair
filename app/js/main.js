4c2a076660affe9adcb6c3ca6fe0d780476c38c3
DenysNazarenko <dinis.nazarenko03@gmail.com>
2024-03-16 12:34:30 +0200

    add section services(not the final option) and updated header

--- app/js/main.js
+++ app/js/main.js
@@ -21,4 +21,57 @@ window.addEventListener('DOMContentLoaded', () => {
       bottomHeader.style.cssText = 'background: transparent; position: static;';
     }
   });
+
+  // dropMenu
+  let arrows = document.querySelectorAll('.menu__arrow');
+  arrows.forEach(arrow => {
+    let subMenu = arrow.nextElementSibling;
+
+    arrow.addEventListener('click', function () {
+      subMenu.classList.toggle('open');
+      arrow.classList.toggle('active');
+    });
+  });
+
+  // Tabs
+  function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
+    const tabs = document.querySelectorAll(tabsSelector),
+      tabsContent = document.querySelectorAll(tabsContentSelector),
+      tabsParent = document.querySelector(tabsParentSelector);
+
+    function hideTabContent() {
+      tabsContent.forEach(item => {
+        item.classList.add('hide');
+        item.classList.remove('show', 'fade');
+      });
+
+      tabs.forEach(item => {
+        item.classList.remove(activeClass);
+      })
+    }
+
+    function showTabContent(i = 0) {
+      tabsContent[i].classList.add('show', 'fade');
+      tabsContent[i].classList.remove('hide');
+      tabs[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+      const target = event.target;
+
+      if (target && target.classList.contains(tabsSelector.slice(1))) {
+        tabs.forEach((item, i) => {
+          if (target == item) {
+            hideTabContent();
+            showTabContent(i);
+          }
+        });
+      }
+    });
+  }
+
+  tabs('.header-tab-services__item', '.content-tab-services__items', '.header-tab-services__items', 'header-tab-services__item_active');
 });
